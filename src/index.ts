a864bff19d096123b78655badb5c767c92168342
Jose Cardona <jpcardonamena@gmail.com>
2024-12-20 17:46:56 +0100

    Added route input validation and swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import clienteRoutes from "./routes/clienteRoutes";
+import {setupSwagger} from "./utils/swagger";
 
 
 const app = express();
 
 app.use(bodyParser.json());
+setupSwagger(app);
 app.use('/clientes', clienteRoutes);
 
 const PORT = 4003;
--- src/routes/clienteRoutes.ts
+++ src/routes/clienteRoutes.ts
@@ -1,43 +1,282 @@
 import express from "express";
 import {body, param} from "express-validator";
 import {
+    createCliente,
+    deleteCliente,
     getAllClientes,
     getClienteByClienteId,
     getClienteByNit,
-    createCliente,
-    updateCliente, deleteCliente
+    updateCliente
 } from "../controllers/clienteController";
+import {authenticateJWT} from "@epehc/sharedutilities/middlewares/authMiddleware";
+import {authorize} from "@epehc/sharedutilities/middlewares/authorize";
+import {UserRole} from "@epehc/sharedutilities/enums/userRole";
 
 const router = express.Router();
 
-router.get('/', getAllClientes);
-router.get('/:cliente_id', getClienteByClienteId);
-router.get('/nit/:nit', getClienteByNit);
+/**
+ * @swagger
+ * tags:
+ *   name: Clientes
+ *   description: API for managing clientes
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Cliente:
+ *       type: object
+ *       required:
+ *         - client_id
+ *         - nombre
+ *         - direccion
+ *         - telefono
+ *         - persona_contacto
+ *         - telefono_persona_contacto
+ *         - email_persona_contacto
+ *         - nit
+ *         - plazas
+ *         - saldo_pendiente
+ *         - saldo_vencido
+ *         - credito_por_dias
+ *       properties:
+ *         client_id:
+ *           type: string
+ *           format: uuid
+ *         nombre:
+ *           type: string
+ *         direccion:
+ *           type: string
+ *         telefono:
+ *           type: string
+ *         persona_contacto:
+ *           type: string
+ *         telefono_persona_contacto:
+ *           type: string
+ *         email_persona_contacto:
+ *           type: string
+ *           format: email
+ *         nit:
+ *           type: string
+ *         plazas:
+ *           type: array
+ *           items:
+ *             type: string
+ *         saldo_pendiente:
+ *           type: number
+ *           format: float
+ *         saldo_vencido:
+ *           type: number
+ *           format: float
+ *         credito_por_dias:
+ *           type: integer
+ */
+
+/**
+ * @swagger
+ * /clientes:
+ *   get:
+ *     summary: Get all clientes
+ *     tags: [Clientes]
+ *     responses:
+ *       200:
+ *         description: List of all clientes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Cliente'
+ */
+router.get('/',
+    authenticateJWT,
+    authorize([UserRole.Admin, UserRole.Reclutador]),
+    getAllClientes);
+
+/**
+ * @swagger
+ * /clientes/{cliente_id}:
+ *   get:
+ *     summary: Get a cliente by ID
+ *     tags: [Clientes]
+ *     parameters:
+ *       - in: path
+ *         name: cliente_id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The cliente ID
+ *     responses:
+ *       200:
+ *         description: Cliente data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Cliente'
+ *       404:
+ *         description: Cliente not found
+ */
+router.get('/:cliente_id',
+    authenticateJWT,
+    authorize([UserRole.Admin, UserRole.Reclutador]),
+    [
+        param('cliente_id').isUUID().withMessage('Cliente ID es requerido')
+    ],
+    getClienteByClienteId);
+
+/**
+ * @swagger
+ * /clientes/nit/{nit}:
+ *   get:
+ *     summary: Get a cliente by NIT
+ *     tags: [Clientes]
+ *     parameters:
+ *       - in: path
+ *         name: nit
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The cliente NIT
+ *     responses:
+ *       200:
+ *         description: Cliente data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Cliente'
+ *       404:
+ *         description: Cliente not found
+ */
+router.get('/nit/:nit',
+    authenticateJWT,
+    authorize([UserRole.Admin, UserRole.Reclutador]),
+    [
+        param('nit').isString().notEmpty().withMessage('NIT es requerido')
+    ],
+    getClienteByNit);
+
+/**
+ * @swagger
+ * /clientes:
+ *   post:
+ *     summary: Create a new cliente
+ *     tags: [Clientes]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Cliente'
+ *     responses:
+ *       201:
+ *         description: Cliente created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Cliente'
+ *       400:
+ *         description: Invalid input
+ */
 router.post(
     '/',
+    authenticateJWT,
+    authorize([UserRole.Admin, UserRole.Reclutador]),
     [
         body('cliente_id').isUUID(),
-        body('nombre').isString(),
-        body('nit').isString(),
-        body('direccion').isString(),
-        body('telefono').isString(),
-        body('correo').isEmail(),
+        body('nombre').isString().notEmpty().withMessage('Nombre es requerido'),
+        body('direccion').isString().notEmpty().withMessage('Direccion es requerida'),
+        body('telefono').isString().notEmpty().withMessage('Telefono es requerido'),
+        body('persona_contacto').isString().notEmpty().withMessage('Persona de contacto es requerida'),
+        body('telefono_persona_contacto').isString().notEmpty().withMessage('Telefono de contacto es requerido'),
+        body('email_persona_contacto').isEmail().notEmpty().withMessage('Correo de contacto es requerido'),
+        body('nit').isString().notEmpty().withMessage('NIT es requerido'),
+        body('plazas').isArray(),
+        body('saldo_pendiente').isFloat(),
+        body('saldo_vencido').isFloat(),
+        body('credito_por_dias').isInt()
     ],
     createCliente
 );
+
+
+/**
+ * @swagger
+ * /clientes/{cliente_id}:
+ *   put:
+ *     summary: Update a cliente by ID
+ *     tags: [Clientes]
+ *     parameters:
+ *       - in: path
+ *         name: cliente_id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The cliente ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Cliente'
+ *     responses:
+ *       200:
+ *         description: Cliente updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Cliente'
+ *       404:
+ *         description: Cliente not found
+ *       400:
+ *         description: Invalid input
+ */
 router.put(
     '/:cliente_id',
+    authenticateJWT,
+    authorize([UserRole.Admin, UserRole.Reclutador]),
     [
         param('cliente_id').isUUID(),
-        body('nombre').optional().isString(),
-        body('nit').optional().isString(),
-        body('direccion').optional().isString(),
-        body('telefono').optional().isString(),
-        body('correo').optional().isEmail(),
+        body('nombre').optional().isString().notEmpty().withMessage('Nombre es requerido'),
+        body('direccion').optional().isString().notEmpty().withMessage('Direccion es requerida'),
+        body('telefono').optional().isString().notEmpty().withMessage('Telefono es requerido'),
+        body('persona_contacto').optional().isString().notEmpty().withMessage('Persona de contacto es requerida'),
+        body('telefono_persona_contacto').optional().isString().notEmpty().withMessage('Telefono de contacto es requerido'),
+        body('email_persona_contacto').optional().isEmail().notEmpty().withMessage('Correo de contacto es requerido'),
+        body('nit').optional().isString().notEmpty().withMessage('NIT es requerido'),
+        body('plazas').optional().isArray(),
+        body('saldo_pendiente').optional().isFloat(),
+        body('saldo_vencido').optional().isFloat(),
+        body('credito_por_dias').optional().isInt()
     ],
     updateCliente
 );
 
-router.delete('/:cliente_id', deleteCliente);
+/**
+ * @swagger
+ * /clientes/{cliente_id}:
+ *   delete:
+ *     summary: Delete a cliente by ID
+ *     tags: [Clientes]
+ *     parameters:
+ *       - in: path
+ *         name: cliente_id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The cliente ID
+ *     responses:
+ *       200:
+ *         description: Cliente deleted
+ *       404:
+ *         description: Cliente not found
+ */
+router.delete('/:cliente_id',
+    authenticateJWT,
+    authorize([UserRole.Admin]),
+    [
+        param('cliente_id').isUUID().withMessage('Cliente ID es requerido')
+    ],
+    deleteCliente);
 
 export default router;
