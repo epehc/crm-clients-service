8f2be1a7d7d196a1743942c936d5fe151bce5233
Jose Cardona <jpcardonamena@gmail.com>
2025-02-07 15:42:20 +0100

    added cors, and persona contacto routes

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,23 @@ import express from "express";
 import bodyParser from "body-parser";
 import clienteRoutes from "./routes/clienteRoutes";
 import {setupSwagger} from "./utils/swagger";
+import cors from "cors";
+import personaContactoRoutes from "./routes/personaContactoRoutes";
 
 
 const app = express();
+const corsOptions = {
+    origin: process.env.FRONTEND_URL, //frontend's URL
+    credentials: true, // Allow credentials (cookies, headers)
+};
 
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 setupSwagger(app);
 app.use('/clientes', clienteRoutes);
+app.use('/personas-contacto', personaContactoRoutes);
 
-const PORT = 4003;
+const PORT = process.env.PORT || 4003;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
