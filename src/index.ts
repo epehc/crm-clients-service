53435e6809985bcc19f47b7059b1ac1181b97fe5
Jose Cardona <jpcardonamena@gmail.com>
2025-02-19 21:12:42 +0100

    Minor changes for deployment

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ setupSwagger(app);
 app.use('/clientes', clienteRoutes);
 app.use('/personas-contacto', personaContactoRoutes);
 
-const PORT = process.env.PORT || 4003;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
